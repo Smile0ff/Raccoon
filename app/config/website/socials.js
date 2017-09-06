c49eb3d605e83d9a7f934294675c6a2a62459f20
Smile0ff <a.parneta@gmail.com>
2017-09-06 17:22:02 +0300

    deploy

--- app/config/website/socials.js
+++ app/config/website/socials.js
@@ -2,22 +2,22 @@ const socials = [
     {
         name: 'facebook',
         icon: 'icon-facebook',
-        link: '#'
+        link: 'https://www.facebook.com/RaccoonTechnologies/'
     },
     {
         name: 'instagram',
         icon: 'icon-instagram',
-        link: '#'
+        link: 'https://www.instagram.com/raccoon.world/'
     },
     {
         name: 'youtube',
         icon: 'icon-youtube',
-        link: '#'
+        link: 'https://www.youtube.com/channel/UC5viqnQVTbaGNSfn5Czkujw'
     },
     {
         name: 'medium',
         icon: 'icon-medium',
-        link: '#'
+        link: 'https://medium.com/@raccoon.world'
     },
 ];
 
