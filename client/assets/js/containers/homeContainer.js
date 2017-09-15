fb5b982b3bb6f68cbe09b9b94faf563f9f298ff1
Smile0ff <a.parneta@gmail.com>
2017-09-15 11:30:24 +0300

    cut intro animation

--- client/assets/js/containers/homeContainer.js
+++ client/assets/js/containers/homeContainer.js
@@ -1,17 +1,9 @@
-import ClipAnimation from '@components/introClip/clipAnimation';
-
 import ToggleMenu from '@components/toggleMenu';
 import Presentation from '@components/presentation';
-
-import isMobile from '@utils/isMobile';
 
 class HomeContainer{
 
     constructor(){
-
-        if(!isMobile())
-            this.clipAnimation = new ClipAnimation();
-        
         this.toggleMenu = new ToggleMenu();
         this.presentation = new Presentation();
             
--- client/assets/js/pages/home.js
+++ client/assets/js/pages/home.js
@@ -1,9 +1,7 @@
 import HomeContainer from '@containers/homeContainer';
 
 import Loader from '@components/loader';
-import isMobile from '@utils/isMobile';
 
-if(isMobile())
-    new Loader();
+new Loader();
 
 window.addEventListener('load', () => new HomeContainer());
